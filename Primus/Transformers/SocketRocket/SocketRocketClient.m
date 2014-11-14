7d213b470b4ec0684d2a11b7bbbafe5b274cf721
katsuhisaishii <katsuhisaishii@katsuhisa-no-MacBook-Pro.local>
2014-11-14 11:06:28 +0900

    pingが送れないバグを修正

--- Primus/Transformers/SocketRocket/SocketRocketClient.m
+++ Primus/Transformers/SocketRocket/SocketRocketClient.m
@@ -76,9 +76,14 @@ typedef NS_ENUM(NSInteger, SocketRocketErrorCode) {
     @try {
 //        [_socket send:data];
         //バイナリのまま送ると送信できなかったので、UTF8に変換してからsendメソッドを呼ぶように修正
-        NSString *string = [[NSString alloc] initWithData:data
-                                                 encoding:NSUTF8StringEncoding];
-        [_socket send:string];
+        if( [data isKindOfClass:[NSString class]] ){
+            [_socket send:data];
+        }
+        else{
+            NSString *string = [[NSString alloc] initWithData:data
+                                                     encoding:NSUTF8StringEncoding];
+            [_socket send:string];
+        }
     }
     @catch (NSException *exception) {
         [_primus emit:@"incoming::error", exception];
